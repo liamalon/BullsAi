9bf74032f3fb7be83b8ebdb01067a879ee0c946f
LiamAlon <liamalon3@gmail.com>
2023-04-29 17:34:30 +0300

    Changed fire time rate

--- MainComputerServer.py
+++ MainComputerServer.py
@@ -12,7 +12,7 @@ PORT: int = 8888
 CODE_LEN: int = 5
 STEP_SIZE: int = 1
 NUM_FRAMES_TO_DETECT: int = 5
-NUM_FRAMES_TO_DETECT_TO_FIRE: int = 6
+NUM_FRAMES_TO_DETECT_TO_FIRE: int = 12
 
 class ImageDetection:
     """
